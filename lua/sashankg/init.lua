e55aee5a45ed9a7d31fff96c82c630331239d5e3
Sashank Gogula <sashank.gogula@glean.com>
2023-05-21 11:35:54 -0700

    Switch to kickstart.nvim

--- lua/sashankg/init.lua
+++
@@ -1,5 +0,0 @@
-require("sashankg.remap")
-require("sashankg.vim")
-
-vim.api.nvim_create_user_command('PR', '!git push; gh pr create -w', {})
-
--- lua/sashankg/remap.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set('n', '<C-h>', '<C-W><C-h>')
-vim.keymap.set('n', '<C-j>', '<C-W><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-W><C-k>')
-vim.keymap.set('n', '<C-l>', '<C-W><C-l>')
-
