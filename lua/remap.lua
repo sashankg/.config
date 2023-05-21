ffd2695fa52989a222cdcaae983a4b7f7305320d
Sashank Gogula <sashank.gogula@glean.com>
2023-05-21 12:54:27 -0700

    Add remap.lua and vim.lua

---
+++ lua/remap.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Navigation
+vim.keymap.set('n', '<C-h>', '<C-W><C-h>', { desc = 'Cursor to left pane' })
+vim.keymap.set('n', '<C-j>', '<C-W><C-j>', { desc = 'Cursor to lower pane' })
+vim.keymap.set('n', '<C-k>', '<C-W><C-k>', { desc = 'Cursor to upper pane' })
+vim.keymap.set('n', '<C-l>', '<C-W><C-l>', { desc = 'Cursor to right pane' })
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
