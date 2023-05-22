55372f64d1b2ed9d1c0cc8bc78565ddf5e0b5139
Sashank Gogula <sashank.gogula@glean.com>
2023-05-22 00:37:14 -0700

    Fix surround lazy load

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,7 +20,7 @@ require('lazy').setup({
   -- Git related plugins
   { 'tpope/vim-fugitive', lazy = true, cmd = { "Git", "G", "Gbrowse" }, },
   'tpope/vim-rhubarb',
-  { 'tpope/vim-surround', lazy = true, keys = { "S" } },
+  { 'tpope/vim-surround', lazy = true, keys = { { "S", mode = "v" } } },
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
