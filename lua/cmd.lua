386d7df17592d7e86b7fbd5781d1021222b70508
Sashank Gogula <sashank.gogula@glean.com>
2023-05-29 03:29:58 -0700

    capitalize custom user commands

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_create_user_command('gundo', function()
+vim.api.nvim_create_user_command('Gundo', function()
   vim.cmd('G reset HEAD~')
 end, {})
 
-vim.api.nvim_create_user_command('gpr', '!git push; gh pr create -w', {})
+vim.api.nvim_create_user_command('Gpr', '!git push; gh pr create -w', {})
 
-vim.api.nvim_create_user_command('gpf', '!git push --force', {})
+vim.api.nvim_create_user_command('Gfp', '!git push --force', {})
