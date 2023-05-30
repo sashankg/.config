b6de01bc62b094b837eebfd921e695bb16f22500
Sashank Gogula <sashank.gogula@glean.com>
2023-05-29 18:59:56 -0700

    rearrange plugin order

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,29 +15,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
   -- Git related plugins
   { 'tpope/vim-fugitive', lazy = true, cmd = { "Git", "G", "Gbrowse" }, },
   'tpope/vim-rhubarb',
-  { 'tpope/vim-surround', lazy = true, keys = { { "S", mode = "v" }, "ds", "cs" } },
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  require('plugins.lspconfig'),
-
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets' },
-  },
-
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',    opts = {} },
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -46,45 +26,64 @@ require('lazy').setup({
       signs = {
         add = { text = '+' },
         change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
       },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = true,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
       on_attach = function(bufnr)
-        vim.keymap.set('n', '[c', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
-        vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        local gitsigns = require('gitsigns')
+        vim.keymap.set('n', '[c', gitsigns.prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
+        vim.keymap.set('n', ']c', gitsigns.next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
+        vim.keymap.set('n', '<leader>gh', gitsigns.preview_hunk, { buffer = bufnr, desc = '[Git] Preview [H]unk' })
+        vim.keymap.set('n', '<leader>gl', function()
+            gitsigns.blame_line { full = true, ignore_whitespace = true }
+          end,
+          { buffer = bufnr, desc = '[G]it B[l]ame' })
       end,
     },
   },
 
+  -- Editing utilities
+  { 'tpope/vim-surround', lazy = true, keys = { { "S", mode = "v" }, "ds", "cs" } },
+  'preservim/nerdcommenter',
+  'Raimondi/delimitMate',
+  'tpope/vim-sleuth',
+  'github/copilot.vim',
+
+  -- Language utilities
+  require('plugins.lspconfig'),
+  require('plugins.formatter'),
+  require('plugins.treesitter'),
   {
-    -- Theme inspired by Atom
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets' },
+  },
+
+  -- UI
+  require('plugins.lualine'),
+  require('plugins.telescope'),
+  { 'folke/which-key.nvim', opts = {} },
+  {
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
     end,
   },
-  require('plugins.lualine'),
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `luka.-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
     },
   },
-  { 'preservim/nerdcommenter', },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  require('plugins.telescope'),
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
   {
     'nvim-telescope/telescope-fzf-native.nvim',
     -- NOTE: If you are having trouble with this installation,
@@ -95,8 +94,4 @@ require('lazy').setup({
     end,
   },
 
-  require('plugins.treesitter'),
-  'github/copilot.vim',
-  require('plugins.formatter'),
-  'Raimondi/delimitMate',
 }, {})
