29c9f5c2bfb4f97787ffbaa2eea31cfb15817ab7
Sashank Gogula <sashank.gogula@glean.com>
2023-05-29 21:32:26 -0700

    add session lens

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -99,6 +99,11 @@ require('lazy').setup({
       auto_save_enabled = true,
       auto_restore_enabled = true,
       auto_session_use_git_branch = true
-    }
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>fs", require("auto-session.session-lens").search_session, {
+        desc = "[Find] [S]ession"
+      })
+    end
   },
 }, {})
