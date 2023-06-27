ba664cda43488358bf55e9d096a37b1d3d16cddc
Sashank Gogula <sashank.gogula@glean.com>
2023-06-26 21:03:49 -0700

    refresh fugitive on enter

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -5,3 +5,8 @@ end, {})
 vim.api.nvim_create_user_command('Gpr', '!git push; gh pr create -w', {})
 
 vim.api.nvim_create_user_command('Gfp', '!git push --force', {})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'ShellCmdPost' }, {
+  pattern = { "fugitive://*/.git//" },
+  command = ":G"
+})
