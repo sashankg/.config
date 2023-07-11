c64662f4eb0db082b69fc830bd86bebed442fce5
Sashank Gogula <sashank.gogula@glean.com>
2023-07-10 23:49:17 -0700

    remove clipboard sync

--- lua/vim.lua
+++ lua/vim.lua
@@ -10,11 +10,6 @@ vim.wo.relativenumber = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
 -- Enable break indent
 vim.o.breakindent = true
 
