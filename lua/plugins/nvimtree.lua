2b6baa4433e096e89a85a096c0805b90ebead536
Sashank Gogula <sashank.gogula@glean.com>
2023-11-12 20:30:34 -0800

    lazy load nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,13 +3,15 @@ return {
   config = true,
   init = function()
     vim.keymap.set("n", "<C-t>", ':NvimTreeFindFile<CR>', {
-      desc = "[Find] [S]ession"
+      desc = "Open file tree"
     })
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'NvimTree',
       callback = function(ev)
-        vim.keymap.set('n', '<C-t>', ':NvimTreeClose<CR>', { desc = 'Close Git status', buffer = ev.buf })
+        vim.keymap.set('n', '<C-t>', ':NvimTreeClose<CR>', { desc = 'Close file tree', buffer = ev.buf })
       end
     })
-  end
+  end,
+  lazy = true,
+  cmd = { "NvimTree", "NvimTreeFindFile" },
 }
