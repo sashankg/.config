7196021566dc1fcf63ef158490e30f1e7a966981
Sashank Gogula <sashank.gogula@glean.com>
2023-11-12 20:17:21 -0800

    move diagnostic keymaps to remap.lua

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 -- Pane navigation
 vim.keymap.set('n', '<C-h>', '<C-W><C-h>', { desc = 'Cursor to left pane' })
 vim.keymap.set('n', '<C-j>', '<C-W><C-j>', { desc = 'Cursor to lower pane' })
