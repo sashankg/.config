4935020fcfd6a182fd385bd739f78e702ae899c4
Sashank Gogula <sashank.gogula@glean.com>
2024-05-23 23:24:37 -0500

    use onedark darker

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -46,6 +46,9 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
       vim.cmd.colorscheme 'onedark'
     end,
   },
